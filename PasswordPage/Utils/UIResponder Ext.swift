6fb067e8b75705f79a51c1802ce2b0731acb2fce
Couti Darius <coutidarius@gmail.com>
2023-09-28 09:32:51 +0300

    feat: handle view adjustments for keyboard

---
+++ PasswordPage/Utils/UIResponder Ext.swift
@@ -0,0 +1,27 @@
+//
+//  UIResponder Ext.swift
+//  PasswordPage
+//
+//  Created by Darius Couti on 28.09.2023.
+//
+
+import UIKit
+
+extension UIResponder {
+
+    private struct Static {
+        static weak var responder: UIResponder?
+    }
+
+    /// Finds the current first responder
+    /// - Returns: the current UIResponder if it exists
+    static func currentFirst() -> UIResponder? {
+        Static.responder = nil
+        UIApplication.shared.sendAction(#selector(UIResponder._trap), to: nil, from: nil, for: nil)
+        return Static.responder
+    }
+
+    @objc private func _trap() {
+        Static.responder = self
+    }
+}
